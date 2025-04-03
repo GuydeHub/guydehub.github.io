d3cdf3b69eba9c3744aa47e9fd5e6c861389c4ce
Guyde-Dev-BETAS <nikko.c.turner@msd549c.org>
2025-04-03 11:07:48 -0700

    Create Notifications.js

---
+++ Notifications.js
@@ -0,0 +1,19 @@
+function SendNotif(text) {
+  var notif = document.createElement("div")
+  notif.style.height = "225px"
+  notif.style.width = "350px"
+  notif.style.backgroundColor = "grey"
+  var closeButton = document.createElement("button")
+  notif.appendChild(closeButton)
+  closeButton.innerHTML = "Close"
+  closeButton.addEventListener("click", function() {
+    document.body.removeChild(notif)
+  })
+  var label = document.createElement("h1")
+  label.innerHTML = text
+  label.style.fontFamily = "cursive"
+  notif.style.textAlign = "center"
+  notif.appendChild(label)
+  document.body.appendChild(notif)
+}
+SendNotif("test")
