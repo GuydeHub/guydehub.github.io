61841b7ce8199479ea405c78aaaf8c644bf7a26d
Guyde-Dev-BETAS <nikko.c.turner@msd549c.org>
2025-04-03 17:32:02 -0700

    Update killswitchvalue.lua

--- killswitchvalue.lua
+++ killswitchvalue.lua
@@ -1,1 +1,10 @@
-return {value = true, reason="Disabled due to an update."}
+return {value = false, reason="None"}
+--[[
+REASONS
+- Disabled due to an update.
+- One of the scripts is currently detected.
+- Script is broken.
+- Disabled for maintenance.
+- Disabled for heavy maintenance.
+- None
+]]
